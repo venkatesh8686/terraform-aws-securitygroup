f609459ec887234fa25b492bcce8aab1ddae023b
venkatesh8686 <83954722+venkatesh8686@users.noreply.github.com>
2024-10-08 17:51:07 +0530

    adding all files

---
+++ locals.tf
@@ -0,0 +1,3 @@
+locals {
+    sg_final_name = "${var.project_name}-${var.environmemt_name}-${var.sg_name}"
+}
---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "project_name" {
+
+}
+
+variable "environmemt_name" {
+
+}
+
+variable "sg_name" {
+    
+}
+variable "comman_tags" {
+    default = {}
+
+}
+
+variable "sg_tags" {
+    default = {}
+}
+
+variable "vpc_id" {
+    
+}
